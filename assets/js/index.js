3bce5d466212028322d9cf6c0c1f7bdee0bd8890
sagunjaiswal <sagunjaiswal99@gmail.com>
2022-06-06 15:28:30 +0530

    completion

---
+++ assets/js/index.js
@@ -0,0 +1,44 @@
+
+
+$("#add_user").submit(function(event){
+    alert("Data inserted successfully");
+})
+$("#update_user").submit(function(event){
+    event.preventDefault();
+
+    var unindexed_arr = $(this).serializeArray();
+    var data = {}
+    $.map(unindexed_arr,function(n,i){
+        data[n['name']] = n['value'];
+    })
+    console.log(unindexed_arr);
+
+    var request = {
+        "url" : `http://localhost:3000/api/users/${data.id}`,
+        "method" : "PUT",
+        "data" : data
+    }
+
+    $.ajax(request).done(function(res){
+        alert("Data updated successfully!");
+    })
+})
+if(window.location.pathname == "/"){
+    $ondelete = $(".table tbody td a.delete");
+    $ondelete.click(function(){
+        var id = $(this).attr("data-id")
+
+        var request = {
+            "url" : `http://localhost:3000/api/users/${id}`,
+            "method" : "DELETE"
+        }
+
+        if(confirm("Do you really want to delete this record?")){
+            $.ajax(request).done(function(response){
+                alert("Data Deleted successfully!");
+                location.reload();
+            })
+        }
+    })
+
+}
